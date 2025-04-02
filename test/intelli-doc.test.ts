121b0e36b52fd5d8f0ed4e500608e683f1d958c5
bugattie <m.mohsinbarkaati@gmail.com>
2025-04-02 14:38:52 +0500

    Initial commit

---
+++ test/intelli-doc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IntelliDoc from '../lib/intelli-doc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/intelli-doc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IntelliDoc.IntelliDocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
